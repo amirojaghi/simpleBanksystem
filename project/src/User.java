f40d7e05fe044dca9d445e01c0bf971c1cb954ff
amirojaghi <amirojaghi35@gmail.com>
2025-05-09 21:50:46 +0330

    Update Account.java

--- project/src/User.java
+++ project/src/User.java
@@ -1,37 +1,70 @@
-public class User {
-    private String name;
-    private double balance;
-    private final int id;
-    private final int password;
+public class Account {
+    private String firstName;
+    private String lastName;
+    private int id;
+    private int pin;
+    private double rialBalance;
+    private double dollarBalance;
+    private static final double EXCHANGE_RATE = 80.0;
 
-    public User(String name, int id, int password) {
-        this.name = name;
+    public Account(String firstName, String lastName, int pin, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pin = pin;
         this.id = id;
-        this.password = password;
-        this.balance = 0;
+        this.rialBalance = 0;
+        this.dollarBalance = 0;
     }
 
-    public String getName() { return name; }
-    public double getBalance() { return balance; }
-    public int getId() { return id; }
-    public int getPassword() { return password; }
-
-    public void setName(String name) { this.name = name; }
-    public void setBalance(double balance) { this.balance = balance; }
-
-    public void deposit(double amount) {
-        balance += amount;
+    public int getId() {
+        return id;
     }
 
-    public boolean withdraw(double amount) {
-        if (amount > balance) {
-            return false;
+    public boolean checkPin(int inputPin) {
+        return this.pin == inputPin;
+    }
+
+    public void depositRial(double amount) {
+        if (amount > 0) {
+            rialBalance += amount;
+            System.out.println(amount + " Rial deposited.");
+        } else {
+            System.out.println("Invalid amount.");
         }
-        balance -= amount;
-        return true;
     }
 
-    public void printBalance() {
-        System.out.printf("Name: %s, ID: %d, Balance: %.2f\n", name, id, balance);
+    public void depositDollar(double amount) {
+        if (amount > 0) {
+            dollarBalance += amount;
+            System.out.println(amount + " USD deposited.");
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    public boolean withdrawRial(double amount) {
+        if (amount <= rialBalance) {
+            rialBalance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean withdrawDollar(double amount) {
+        if (amount <= dollarBalance) {
+            dollarBalance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void showInfo() {
+        System.out.println("\n--- Account Information ---");
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Account ID: " + id);
+        System.out.println("Rial Balance: " + rialBalance);
+        System.out.println("USD Balance: $" + dollarBalance);
+        System.out.println("Total in Rial: " + (rialBalance + dollarBalance * EXCHANGE_RATE));
+        System.out.println("Total in USD: $" + (dollarBalance + rialBalance / EXCHANGE_RATE));
     }
 }
